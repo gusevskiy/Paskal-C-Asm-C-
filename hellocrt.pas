190ba9ffedfcd8b808e13e3bb794aeddbcef29da
gusevskiy <vlgu@mail.ru>
2022-05-20 19:40:10 +0300

    динамический ввод

--- hellocrt.pas
+++ hellocrt.pas
@@ -1,19 +1,19 @@
 program HelloCrt;
 
-uses crt;
+uses crt;  {вызов модуля}
 
 const
-    Message = 'Hello, world!';
-    DelayDuration = 5000; { 5 seconds }
+    Message = 'Hello, world!';               {определение констант}
+    DelayDuration = 5000;                    { 5 seconds }
 var
-    x, y: integer;
+    x, y: integer;                           {обьявление переменных}
 begin
-    clrscr;
-    x := ( ScreenWidth - length(Message)) div 2;
-    y := ScreenHeight div 2;
-    GotoXY(x,y);
-    write(Message);
-    GotoXY(1, 1);
-    delay(DelayDuration);
-    clrscr
+    clrscr;                                  {процедура очистки экрана}
+    x := ( ScreenWidth - length(Message)) div 2; {определение координаты по ширене}
+    y := ScreenHeight div 2;             {определение координаты по высоте}
+    GotoXY(x,y);                         {перемещение курсора на координаты}
+    write(Message);                      {вывод сообщения}
+    GotoXY(1, 1);                        {перемещение курсора в координаты}
+    delay(DelayDuration);                 {задержка переж закрытием}
+    clrscr                                {очистка экрана}
 end.
