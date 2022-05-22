72e686ce0c596847a2cd51d0fad8dc0f4635b899
gusevskiy <vlgu@mail.ru>
2022-05-22 22:20:09 +0300

    запись в файл с перехватом ошибок

--- hellofile.pas
+++ hellofile.pas
@@ -1,13 +1,24 @@
 program HelloFile;
 
 const
-    message = 'Hello, world!';
+    message = 'Hello, world!!!!!';
     filename = 'hello.txt';
 var
     f: text;
 begin
+    {$I-}
     assign(f, filename);
     rewrite(f);
+    if IOResult <> 0 then
+    begin
+        writeln('Couldn''t open file ', filename);
+        halt(1)
+    end;
     writeln(f, message);
+    if IOResult <> 0 then
+    begin
+        writeln('Couldn''t open file ', filename);
+        halt(1)
+    end;
     close(f)
 end.
