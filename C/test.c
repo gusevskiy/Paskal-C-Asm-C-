43834a4ec878748a66c7ad136d13d184021117c8
gusevskiy <vlgu@mail.ru>
2022-05-26 18:48:39 +0300

    муторно

--- C/test.c
+++ C/test.c
@@ -4,6 +4,12 @@
 int main() /*int-тип возвращаемого значения*/
 {
     int a = 5;
-    printf("%d\n", a * 2);
+    int i = 5;
+    int x = a++;
+    printf("%d %d\n", a, x);
+    for (i = 1; i <= 25; i++)           
+    {
+        printf("%d x %d\t = %d\n", i, i, i*i);
+    }
     return 0;
 }
