e911913d72599ece4bdcb3fff4d51d6dfd668fb3
gusevskiy <vlgu@mail.ru>
2022-05-26 13:44:21 +0300

    Будет не просто

---
+++ C/Hello_World.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() /*int-тип возвращаемого значения*/
+{
+    printf("Hello, world!\n");
+    return 0;
+}
---
+++ C/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() /*int-тип возвращаемого значения*/
+{
+    int a = 5;
+    printf("%d\n", a * 2);
+    return 0;
+}
